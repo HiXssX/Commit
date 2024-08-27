cfa4aa44c3db22af7c1f29a3ce08e5d328f686f4
unknown <unknown>
2024-08-27 12:19:02 -0300

    tabela de idiomas

---
+++ VersionSQL/HigorPC/crm/dbo/Table/idiomas.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Table [dbo].[idiomas]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE TABLE [dbo].[idiomas](
+	[id_cliente] [int] NOT NULL,
+	[idioma] [varchar](50) NULL
+) ON [PRIMARY]
+
+ALTER TABLE [dbo].[idiomas]  WITH CHECK ADD FOREIGN KEY([id_cliente])
+REFERENCES [dbo].[cliente] ([id_cliente])
